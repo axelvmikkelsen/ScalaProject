fe056b40f202956469325277e768958c0365266a
axelvmikkelsen <axelvmikkelsen@gmail.com>
2019-10-21 13:48:55 +0200

    1c

--- delivery/tasks/task1.scala
+++ delivery/tasks/task1.scala
@@ -4,9 +4,6 @@ object Main extends App {
     /** 1a **/
     var numarray = new Array[Int](50)
     var output = "["
-
-    var end = false
-
     var iter = 1
 
     for (a <- 1 to 50) {
@@ -31,4 +28,17 @@ object Main extends App {
       return sum
     }
     println(sumList(numarray))
+
+
+    /** 1c **/
+    def recursiveSumList(list:Array[Int], sum:Int) : Int = {
+      var len: Int = list.length
+      if (len == 0) {
+        return sum
+      }
+      return recursiveSumList(list.drop(1), list(0) + sum)
+    }
+
+    println(recursiveSumList(numarray, 0))
+
 }
